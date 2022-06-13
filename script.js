74afb7a856557db45a333e62bfbff3f7a8ac2d29
toshiaki21 <toshiaki.fujioka21@gmail.com>
2022-06-13 20:12:14 +0900

    pagepiling追加しました

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
- $(function(){
+$(function(){
 
-// // 1,熱中症とは？原因 
+// 1,熱中症とは？原因 
   $(".question").on("click",function(){
-   $(this).next().slideToggle();
+    $(this).next().slideToggle();
     $(this).toggleClass('on');
   })
 
+console.log('Hello world');
 
- })
+$('#pagepiling').pagepiling({
+  menu: '#menu',
+  anchors: ['page1', 'page2', 'page3','page4'],
+  navigation: {
+    'position': 'right',
+    'tooltips': ['section1', 'section2', 'section3', 'section4']
+  },
+  scrollingSpeed: 1000,
+  sectionSelector: '.contents'
+});
+});
+
+
+// 大外のカッコ
+})
