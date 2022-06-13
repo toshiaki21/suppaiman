9c72c3c1957cc3ef485f1be533fb55c92ea19249
kinchan222 <st4el158rm6@gmail.com>
2022-06-13 21:04:16 +0900

    ファイル全部変更しました

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
-$(function(){
+$(function () {
 
-// 1,熱中症とは？原因 
-  $(".question").on("click",function(){
+  // 1,熱中症とは？原因 
+  $(".question").on("click", function () {
     $(this).next().slideToggle();
     $(this).toggleClass('on');
   })
 
+  console.log('Hello world');
 
-$('#pagepiling').pagepiling({
-  menu: '#menu',
-  anchors: ['page1', 'page2', 'page3','page4'],
-  navigation: {
-    'position': 'right',
-    'tooltips': ['section1', 'section2', 'section3', 'section4']
-  },
-  scrollingSpeed: 1000,
-  sectionSelector: '.contents'
-});
-});
+  $('#pagepiling').pagepiling({
+    sectionSelector: '.section',
+    menu: '#menu',
+    // anchors: ['page1', 'page2', 'page3', 'page4', 'page5'],
+    navigation: {
+      'position': 'right',
+      'tooltips': ['section1', 'section2', 'section3', 'section4', 'section5']
+    },
+    scrollingSpeed: 500,
+  });
+
 
 
 // 大外のカッコ
+});
