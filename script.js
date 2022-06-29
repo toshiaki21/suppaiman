28a4d23db4c1c48f38d5cf44bfe28c219be0b03c
kinchan222 <st4el158rm6@gmail.com>
2022-06-30 01:28:52 +0900

    大幅に修正。完成させたいので触ってます

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
-$(document).ready(function() {
-	$('#pagepiling').pagepiling({
-		menu: '#menu',
-	    	anchors: ['page1', 'page2', 'page3','page4','page5'],
-        anchorsColor:"#FFFFFF",
-			sectionsColor: ['#FFFFFF'],
-			navigation: {
-			    'position': 'right',
-			   	'tooltips': ['紺色', '桃色', '黒色']
-		}
+$(function(){
+	$(document).ready(function() {
+		$('#pagepiling').pagepiling({
+			menu: '#menu',
+					anchors: ['page1', 'page2', 'page3','page4','page5'],
+					anchorsColor:"#00bfff",
+				sectionsColor: ['#FFFFFF'],
+				navigation: {
+						'position': 'right',
+						 'tooltips': ['紺色', '桃色', '黒色']
+			}
+		});
 	});
-});
+
+
+	// slick
+	$('.ntu-slick').slick({
+		dots: true,
+		infinite: true,
+		speed: 500,
+		fade: true,
+		cssEase: 'linear'
+	});
+
+
+
+
+
+// 大外かっこ
+})
